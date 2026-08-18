90fdae6570516f52ebab557b4fd2508fb8a4c618
Angel <noelleangel8@gmail.com>
2026-08-19 04:50:41 +0800

    Add files via upload

---
+++ Angel.java
@@ -0,0 +1,15 @@
+class Angel {
+   public static void main (String[] args) {
+      System.out.println("Welcome to Java!");
+      System.out.println("Hello! My name is Maria Angel Noelle Villanueva.");
+      System.out.println("I live at Unit 1204, 12th floor, Cityland 8, Makati City.");
+      System.out.println("I am a first year student in the BSCS1A section.");
+      System.out.println("I was born on January 15, 2004, at 6:23 AM.");
+      System.out.println("I really love watching anime and playing video games.");
+      System.out.println("I can enjoy any genres of music.");
+      System.out.println("I make sure to pray the Rosary to heal my anxiety.");
+      System.out.println("I dream of working in the technological field someday.");
+      System.out.println("I also want to travel to other countries someday.");
+      System.out.println("I hope for the best for all of us.");
+    }
+}
